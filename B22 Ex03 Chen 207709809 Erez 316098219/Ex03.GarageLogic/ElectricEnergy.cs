264c7236a56a10b4d346d2f394371506f9615488
chenberger <chen10.berger@gmail.com>
2022-04-29 18:40:30 +0300

    erez's classes

---
+++ B22 Ex03 Chen 207709809 Erez 316098219/Ex03.GarageLogic/ElectricEnergy.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class ElectricEnergy : Energy
+    {
+        public ElectricEnergy(float i_AmountBatteryTimeInHours, float i_MaximumBatteryTimeInHours) : base(i_AmountBatteryTimeInHours, i_MaximumBatteryTimeInHours) { }
+
+        public void ChargeBattery(float i_HoursToCharge)
+        {
+            if (i_HoursToCharge + CurrentAmountOfEnergy > MaxEnergy)
+            {
+                // throw exception
+            }
+
+            CurrentAmountOfEnergy += i_HoursToCharge;
+        }
+    }
+}
---
+++ B22 Ex03 Chen 207709809 Erez 316098219/Ex03.GarageLogic/Energy.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex03.GarageLogic
+{
+    public abstract class Energy
+    {
+        private float m_CurrentAmountOfEnergy;
+        private readonly float r_MaxEnergy;
+
+        public Energy(float i_AmountOfEnergy, float i_MaxEnergy)
+        {
+            m_CurrentAmountOfEnergy = i_AmountOfEnergy;
+            r_MaxEnergy = i_MaxEnergy;
+        }
+
+        public float CurrentAmountOfEnergy
+        {
+            get
+            {
+                return m_CurrentAmountOfEnergy;
+            }
+
+            set
+            {
+                m_CurrentAmountOfEnergy = value;
+            }
+        }
+
+        public float MaxEnergy
+        {
+            get
+            {
+                return r_MaxEnergy;
+            }
+        }
+    }
+}
