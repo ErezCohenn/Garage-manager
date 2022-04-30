91b8ee5ef41f566ad75fda880ede671a0bf3f516
Erez Cohen <erez6356458@gmail.com>
2022-04-30 12:02:52 +0300

    add ebergy classes
    Signed-off-by: Erez Cohen <erez6356458@gmail.com>

--- B22 Ex03 Chen 207709809 Erez 316098219/Ex03.GarageLogic/FuelEnergy.cs
+++ B22 Ex03 Chen 207709809 Erez 316098219/Ex03.GarageLogic/FuelEnergy.cs
@@ -1,34 +1,29 @@
 ﻿namespace Ex03.GarageLogic
 {
-    public class FuelEnergy : Energy
+    public class FuelEnergy : EnergySource
     {
         public enum eType
         {
             Octan98,
             Octan96,
             Octan95,
-            Soler
+            Soler,
         }
 
         private readonly eType r_FuelType;
-        public FuelEnergy(eType i_FuelType, float i_AmountOfFuelInLiters, float i_MaxAmountOfFuelInLiters) : base(i_AmountOfFuelInLiters, i_MaxAmountOfFuelInLiters)
+        public FuelEnergy(eType i_FuelType, float i_AmountOfFuelInLiters, float i_MaximumAmountOfFuelInLiters) : base(i_AmountOfFuelInLiters, i_MaximumAmountOfFuelInLiters)
         {
             r_FuelType = i_FuelType;
         }
 
-        public void ReFuel(float i_AmountFuelInLitersToLoad, eType i_FuelType)
+        public void ReFuel(float i_AmountFuelInLitersToAdd, eType i_FuelType)
         {
-            if (i_AmountFuelInLitersToLoad + CurrentAmountOfEnergy > MaxEnergy)
-            {
-                // throw exception
-            }
-
             if (i_FuelType != r_FuelType)
             {
                 // throw exception
             }
 
-            CurrentAmountOfEnergy += i_AmountFuelInLitersToLoad;
+            base.LoadEnergy(i_AmountFuelInLitersToAdd);
         }
 
         public eType FuelType
